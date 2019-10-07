a3b3c5c7e09856ba2bc3c9883844f2a91cee0a69
Owoyemi Emmanuel <owoyemi4emmy@gmail.com>
2019-10-07 18:43:36 +0300

    Setting up shared view in layout

---
+++ Kibris Order Software Solutions/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Kibris_Order_Software_Solutions.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
